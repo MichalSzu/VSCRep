c3ba6b3d2fb0f11b6ce4bde2c502187d88ec6bdb
MichalSzu <michal.szulc13@gmail.com>
2019-07-16 22:24:48 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys impoer keys
+import time
+
+class TwitterBot:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
---
+++ hello.py
@@ -0,0 +1,10 @@
+import sys
+import requests
+
+# print (sys.version)
+# print(sys.executable)
+
+r = requests.get('https://google.com')
+print(r.status_code)
+
+print("Hello", name) 
